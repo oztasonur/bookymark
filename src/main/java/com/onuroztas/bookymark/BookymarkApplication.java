6a28a8532051d2dc825b2cf63770c7b33506cbb2
Onur Öztaş <github@onuroztas.com>
2025-01-08 11:31:37 +0300

    Initial commit

---
+++ src/main/java/com/onuroztas/bookymark/BookymarkApplication.java
@@ -0,0 +1,13 @@
+package com.onuroztas.bookymark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookymarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookymarkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onuroztas/bookymark/BookymarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onuroztas.bookymark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookymarkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
